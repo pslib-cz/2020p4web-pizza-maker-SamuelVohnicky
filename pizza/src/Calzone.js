0a60d6b0552ea2eb51f600119ddd4980b6dfa899
SamuelVohnicky <43740161+SamuelVohnicky@users.noreply.github.com>
2021-01-07 16:03:37 +0100

    Initial

---
+++ pizza/src/Calzone.js
@@ -0,0 +1,19 @@
+import './Calzone.css';
+
+<div className="App">
+      <ul className="Navbar">
+        <li><a href="Pizza.js">Pizza</a></li>
+        <li><a href="Calzone.js">Calzone</a></li>
+        <li><a href="App.js">Home</a></li>
+      </ul>
+      <p>Calzone</p>
+
+      <ul>
+            <li><button onclick="CalzoneSunka()">šunka</button></li> 
+            <li><button onclick="CalzoneSalam()">salám</button></li>
+            <li><button onclick="CalzoneSyr()">sýr</button></li>
+            <li><button onclick="CalzoneAnanas()">ananas</button></li>
+            <li><button onclick="CalzoneHouby()">houby</button></li>
+            <li><button onclick="CalzoneChilly()">chilly</button></li>
+        </ul>
+    </div>
